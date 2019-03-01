cd8acef89a7fb98def28ba0cf5d3514be62323dd
Adelice <nancytoniw@gmail.com>
2019-03-01 12:33:38 +0200

    add a blog repository

---
+++ app/models.py
@@ -0,0 +1,101 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255)) 
+    
+
+    @property
+    def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
+   
+    
+    def __repr__(self):
+        return f'User {self.username}'
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))   
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+
+    id = db.Column(db.Integer,primary_key = True)
+    user_id=db.Column(db.Integer,db.ForeignKey("users.id"))
+    description=db.Column(db.String(255))
+    
+    comments= db.relationship('Comment',backref = 'pitch',lazy="dynamic")
+
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+#     @classmethod 
+#     def clear_pitches(cls):
+#         Pitch.search_pitches.clear()
+#     @classmethod
+
+#     def get_pitche(cls,id):
+#         pitche = Pitch.query.filter_by(user_id=id).all()
+#         return pitche
+#     @classmethod
+#     def get_pitches(cls):
+#         pitches=Pitch.query.filter_by().all()
+#         return pitches    
+
+        
+
+
+#     def __repr__(self):
+#         return f'User {self.name}'
+#     pass_secure  = db.Column(db.String(255))
+
+# class Comment(db.Model):
+#     __tablename__ = 'comments'
+#     id = db.Column(db.Integer,primary_key = True)
+#     user_id=db.Column(db.Integer,db.ForeignKey("users.id"))
+#     pitch_id=db.Column(db.Integer, db.ForeignKey("pitch.id"))
+#     content=db.Column(db.String(255))
+    
+#     def save_comment(self):
+#         db.session.add(self)
+#         db.session.commit()
+
+#     @classmethod
+#     def clear_comments(cls):
+#         Comment.all_comments.clear()
+
+#     @classmethod
+#     def get_comments(cls,id):
+#         comments = Comment.query.filter_by(pitch_id=id).all()
+#         return comments
+
+#     @classmethod
+#     def get_commentss(cls,id):
+#         comments = Comment.query.filter_by(user_id=id).all()
+#         return comments
+
+    
+# class PhotoProfile(db.Model):
+#     __tablename__ = 'profile_photos'
+
+#     id = db.Column(db.Integer,primary_key = True)
+#     pic_path = db.Column(db.String())
+#     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    
+
